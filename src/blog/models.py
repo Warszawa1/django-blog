2f60f0ef5d4ca0753421e9368f78e30f89a51a1a
Irene Antelo <iantelovazquez@gmail.com>
2023-02-05 23:03:57 +0100

    Set user on models

--- src/blog/models.py
+++ src/blog/models.py
@@ -1,9 +1,13 @@
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
 
-class BlogPost(models.Model):
+User = settings.AUTH_USER_MODEL
+
+class BlogPost(models.Model): # blogpost_set -> gives you the queryset
     # id = models.IntegerField() # primary key
+    user = models.ForeignKey(User, default=1, null=True, on_delete=models.SET_NULL)
     title = models.CharField(max_length=120)
     slug = models.SlugField(unique=True) # hello world -> hello-world
     content = models.TextField(null=True, blank=True)

296796257db7057da30197a8d37ad14e26e4fb4b
Irene Antelo <iantelovazquez@gmail.com>
2023-02-06 13:02:35 +0100

    update & delete posts

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.admin.views.decorators import staff_member_required
 from django.contrib.auth.decorators import login_required
 from django.http import Http404 # longer version to display 404 errors
@@ -31,6 +31,9 @@ def blog_post_list_view(request):
     qs = BlogPost.objects.all() # queryset -> list of python objects
     # could be search
     # qs = BlogPost.objects.filter(title__icontains='Hola')
+    if request.user.is_authenticated:
+        my_qs = BlogPost.objects.filter(user=request.user)
+        qs = (qs | my_qs).distinct()
     template_name = 'blog/list.html'
     context = {'object_list': qs}
     return render(request, template_name, context)
@@ -40,9 +43,10 @@ def blog_post_list_view(request):
 def blog_post_create_view(request):
     # create objects
     # ? use a form
-    form = BlogPostModelForm(request.POST or None)
+    form = BlogPostModelForm(request.POST or None, request.FILES or None)
     if form.is_valid():
         obj = form.save(commit=False)
+        obj.user = request.user
         obj.save()
         form = BlogPostModelForm()
     template_name = 'form.html'
@@ -58,16 +62,23 @@ def blog_post_detail_view(request, slug):
     return render(request, template_name, context)
 
 
-def blog_post_update_view(request):
+@staff_member_required
+def blog_post_update_view(request, slug):
     obj = get_object_or_404(BlogPost, slug=slug)
-    template_name = 'blog/update.html'
-    context = {"object": obj, 'form': None}
+    form = BlogPostModelForm(request.POST or None, instance=obj)
+    if form.is_valid():
+        form.save()
+    template_name = 'form.html'
+    context = {"title": f"Update {obj.title}", 'form': form}
     return render(request, template_name, context)
 
 
-def blog_post_delete_view(request):
+def blog_post_delete_view(request, slug):
     obj = get_object_or_404(BlogPost, slug=slug)
     template_name = 'blog/delete.html'
+    if request.method == "POST":
+        obj.delete()
+        return redirect("/blog")
     context = {"object": obj}
     return render(request, template_name, context)
 
